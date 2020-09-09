417e407998ee34582a5bfb6d7aefb2856649360b
Aeroraven <70007256+Aeroraven@users.noreply.github.com>
2020-09-09 14:32:55 +0800

    Update main.js
    Results now can be saved as files.

--- EAMSHelper/main.js
+++ EAMSHelper/main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         EAMSHelper
 // @namespace    https://github.com/Aeroraven/Hello_World/EAMSHelper
-// @version      0.1
+// @version      0.2
 // @description  Archive information on EAM System.
 // @author       Aeroraven
 // @match        *
@@ -14,15 +14,18 @@
 function eamsHelper() {
 	eamsHelper.getPointsReportAddr="(Empty)";
 	eamsHelper.getPointsReportCacheAddr="";
-    eamsHelper.standardInterval = 200;
-    eamsHelper.standareSliceLength = 800;
-
+    eamsHelper.standardInterval = 500;
+    eamsHelper.standardSliceLength = 800;
+    eamsHelper.standardFileSliceLength = 5000;
     eamsHelper.callbackAddr = "(Empty)";
+    eamsHelper.callbackType = 0;//0:URL / 1:FileDownload
 
 	this._this=this;
 	this.getPointsReportStatus=0;
     this.getPointsReportTimer = 0;
 
+    this.fileContentArray = 0;
+    this.fileContentArrayCur = 0;
     this.uiProgTimer = 0;
 
     this.curStuId = 0;
@@ -36,7 +39,20 @@ function eamsHelper() {
         temp = null;
         return output;
     } 
+    this.standardFileSave = function (sFileName, sContent) {
+        var virtualDom = document.createElement("a");
+        virtualDom.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(sContent));
+        virtualDom.setAttribute("download", sFileName);
 
+        console.log("SAVE:" + "data:text/plain;charset=utf-8," + encodeURIComponent(sContent));
+        if (virtualDom.createEvent) {
+            var ev = virtualDom.createEvent("MouseEvents");
+            ev.initEvent("click", true, true);
+            virtualDom.dispatchEvent(ev);
+        } else {
+            virtualDom.click();
+        }
+    }
 	this.standardXHRGet = function(sUrl,bAsync,fOnSuccess=function(obj){},fOnError=function(errId){}){
 		var obj=new XMLHttpRequest();
 		var ret;
@@ -59,27 +75,61 @@ function eamsHelper() {
         var t1Dom = document.getElementById("eamsHelper_remoteApi");
         var t2Dom = document.getElementById("eamsHelper_callbackApi");
         t1Dom.innerHTML = eamsHelper.getPointsReportAddr;
-        t2Dom.innerHTML = eamsHelper.callbackAddr;
-    }
-	this.sendCallback = function(cbAddress,cbIdentity,cbData,cbExtra){
-        var place = document.getElementById('eamsHelper_jsonp_placement');
-        var cbLen = cbData.length;
-        var cbSlices = parseInt(cbData.length / eamsHelper.standareSliceLength);
-        console.log("REQLEN=" + cbLen);
-        var cbSlicedData = "";
-        for (var i = 0; i <= cbSlices; i++) {
-            if (i != cbSlices) {
-                cbSlicedData = cbData.substring(i * eamsHelper.standareSliceLength, (i + 1) * eamsHelper.standareSliceLength);
-            } else {
-                cbSlicedData = cbData.substring(i * eamsHelper.standareSliceLength, cbLen);
-            }
-            var script = document.createElement('script');
-            script.src = cbAddress + "?id=" + cbIdentity + "&data=" + cbSlicedData + "&slice=" + i;
-            place.appendChild(script);
+        if (eamsHelper.callbackType != 1) {
+            t2Dom.innerHTML = eamsHelper.callbackAddr;
+        } else {
+            t2Dom.innerHTML = "(Save As File)"
         }
+        
+
+        var clearHost = document.getElementsByTagName("head")[0];
+        clearHost.innerHTML = "";
+    }
+    this.sendCallback = function (cbAddress, cbIdentity, cbData, cbExtra, self=this) {
+        if (eamsHelper.callbackType == 0) {
+            var place = document.getElementById('eamsHelper_jsonp_placement');
+            var cbLen = cbData.length;
+            var cbSlices = parseInt(cbData.length / eamsHelper.standardSliceLength);
+            console.log("REQLEN=" + cbLen);
+            var cbSlicedData = "";
+            for (var i = 0; i <= cbSlices; i++) {
+                if (i != cbSlices) {
+                    cbSlicedData = cbData.substring(i * eamsHelper.standardSliceLength, (i + 1) * eamsHelper.standardSliceLength);
+                } else {
+                    cbSlicedData = cbData.substring(i * eamsHelper.standardSliceLength, cbLen);
+                }
+                var script = document.createElement('script');
+                script.src = cbAddress + "?id=" + cbIdentity + "&data=" + cbSlicedData + "&slice=" + i;
+                place.appendChild(script);
+            }
+        } else if (eamsHelper.callbackType == 1) {
+            self.fileContentArrayCur++;
+            self.fileContentArray[self.fileContentArrayCur] = {};
+            self.fileContentArray[self.fileContentArrayCur]['id'] = cbIdentity;
+            self.fileContentArray[self.fileContentArrayCur]['data'] = cbData;
+
+            
+        }
+        
        
 	}
-	
+    this.saveAsFile = function (self = this) {
+        var saveContent = JSON.stringify(self.fileContentArray);
+        var cbLen = saveContent.length;
+        var cbSlices = parseInt(saveContent.length / eamsHelper.standardFileSliceLength);
+        var cbSlicedData = "";
+        var fileNamePrefix = prompt("Please set name for file to be saved:", "EAMSHelperSave_")
+
+        for (var i = 0; i <= cbSlices; i++) {
+            if (i != cbSlices) {
+                cbSlicedData = saveContent.substring(i * eamsHelper.standardFileSliceLength, (i + 1) * eamsHelper.standardFileSliceLength);
+            } else {
+                cbSlicedData = saveContent.substring(i * eamsHelper.standardFileSliceLength, cbLen);
+            }
+            self.standardFileSave(fileNamePrefix + i+".txt", cbSlicedData);
+        }
+    }
+
 	this.getPointsReportInitiator = function(stIdentity,self=this){
         if (self.getPointsReportStatus==1)return 0;
         self.getPointsReportStatus=1;
@@ -94,6 +144,9 @@ function eamsHelper() {
         var sbData = btoa(unescape(encodeURIComponent(stReponse)));
         sbData = sbData.replace(/[+]/g, "$");
         self.sendCallback(eamsHelper.callbackAddr, stIdentity, sbData, "spr");
+        if (eamsHelper.callbackType == 1 && self.curStuId == self.endStuId) {
+            self.saveAsFile();
+        }
         console.log(sbData);
 	}
 	
@@ -105,6 +158,8 @@ function eamsHelper() {
                     parentObject.getPointsReportInitiator(parentObject.curStuId);
                     if (parentObject.curStuId == stIdUbound) {
                         clearInterval(parentObject.getPointsReportTimer);
+                        alert("Task Completed!");
+                        
                     }
                 }
                 
@@ -125,6 +180,9 @@ function eamsHelper() {
         if (configJson.hasOwnProperty("callback_InterfaceAddr")) {
             eamsHelper.callbackAddr = configJson["callback_InterfaceAddr"];
         }
+        if (configJson.hasOwnProperty("callback_Type")) {
+            eamsHelper.callbackType = configJson["callback_Type"];
+        }
        
     }
 
@@ -133,7 +191,7 @@ function eamsHelper() {
             var descrDom = document.getElementById("eamsHelper_descr");
             var progDom = document.getElementById("eamsHelper_progBar_Process");
             var percentDom = document.getElementById("eamsHelper_prog_percent");
-            var curStat = (self.curStuId - self.begStuId) / (self.endStuId - self.begStuId) * 100;
+            var curStat = (self.curStuId - self.begStuId+1) / (self.endStuId - self.begStuId+1) * 100;
             descrDom.innerHTML = "In Progress. ID:" + thisObj.curStuId;
             progDom.style.width = curStat + "%";
             percentDom.innerHTML = parseInt(curStat) + "%";
@@ -153,8 +211,12 @@ function eamsHelper() {
 
         self.ui_showProg();
         self.batchPointsReport(self.begStuId, self.endStuId);
+        self.fileContentArray = {};
     }
     this.ui_prestart_json = function (self = this) {
+        if (!confirm("Launching EAMSHelper will completely remove almost all elements in the page. The removal is unchangeable until you reload the page. Are you sure to launch EAMSHelper?")) {
+            return;
+        }
         var confstr = prompt("Input configuration JSON string below:", "{}");
         this.loadConfigurations(confstr, false);
         this.pageInject();
@@ -168,6 +230,9 @@ function eamsHelper() {
         var injectStr = atob("Jmx0O2RpdiBpZD0iZWFtc0hlbHBlclByZWxvYWRlcl9pbmplY3Rvcl9tYWluIiBzdHlsZT0iYmFja2dyb3VuZC1jb2xvcjojMDAwMDAwO2NvbG9yOiNmZmZmZmY7bGVmdDowcHg7cmlnaHQ6MHB4O3RvcDowcHg7cG9zaXRpb246Zml4ZWQ7bWFyZ2luLWxlZnQ6MHB4O21hcmdpbi1yaWdodDowcHg7bWFyZ2luLXRvcDowcHg7bWFyZ2luLWJvdHRvbTowcHg7Zm9udC1zaXplOjE3cHg7IiZndDsNCgkmbHQ7c2NyaXB0Jmd0Ow0KCQkvL0VBTVNIZWxwZXIgQ2FsbGVyDQoJCQ0KCSZsdDsvc2NyaXB0Jmd0Ow0KCSZsdDtkaXYgaWQ9ImVhbXNIZWxwZXJQcmVsb2FkZXJfanNvbnBfcGxhY2VtZW50IiZndDsNCgkmbHQ7L2RpdiZndDsNCgkmbHQ7c3R5bGUmZ3Q7DQoJLmVhbXNIZWxwZXJfY2xfZXhwbHsNCgkJY29sb3I6I2FhYWFhYTsNCgkJZm9udC1zaXplOjE0cHg7DQoJCXBhZGRpbmctbGVmdDoxMHB4Ow0KCQltYXJnaW4tdG9wOjhweDsNCgkJZGlzcGxheTpibG9jazsNCgl9DQoJLmVhbXNfY2xfYnRuMnsNCgkJZm9udC1zaXplOjE0cHg7DQoJCWJvcmRlci1yYWRpdXM6NHB4Ow0KCQliYWNrZ3JvdW5kLWNvbG9yOiM0MDhkZWE7DQoJCWNvbG9yOiNmZmZmZmY7DQoJCWJvcmRlcjowcHg7DQoJCXBhZGRpbmctdG9wOjEwcHg7DQoJCXBhZGRpbmctYm90dG9tOjEwcHg7DQoJCXBhZGRpbmctbGVmdDoyMHB4Ow0KCQlwYWRkaW5nLXJpZ2h0OjIwcHg7DQoJCW1hcmdpbi1yaWdodDo1cHg7DQoJfQ0KCS5lYW1zSGVscGVyX2NsX2lucHV0ew0KCQlib3JkZXItdG9wOjBweDsNCgkJYm9yZGVyLWxlZnQ6MHB4Ow0KCQlib3JkZXItcmlnaHQ6MHB4Ow0KCQliYWNrZ3JvdW5kLWNvbG9yOiMwMDAwMDA7DQoJCWJvcmRlci1ib3R0b206MnB4ICMyMDZkY2Egc29saWQ7DQoJCWNvbG9yOiNmZmZmZmY7DQoJCWZvbnQtc2l6ZToyMHB4Ow0KCX0NCgkuZWFtc0hlbHBlcl9jbF9sYWJlbHsNCgkJY29sb3I6IzQwOGRlYTsNCgl9DQoJJmx0Oy9zdHlsZSZndDsNCgkmbHQ7ZGl2IGlkPSJlYW1zSGVscGVyUHJlbG9hZGVyX3RpdGxlIiBzdHlsZT0iYmFja2dyb3VuZC1jb2xvcjojMjIyMjIyO2NvbG9yOiM0MDhkZWE7cGFkZGluZy10b3A6MTVweDtwYWRkaW5nLWJvdHRvbToxNXB4O3BhZGRpbmctbGVmdDoxNXB4O2ZvbnQtc2l6ZToxNHB4O2ZvbnQtZmFtaWx5OmJhaG5zY2hyaWZ0O3otaW5kZXg6OTk5OTk7IiZndDtUbyBsYXVuY2ggRUFNU0hlbHBlciwgQ2xpY2sgaGVyZTogJm5ic3A7Jm5ic3A7DQoJCSZsdDtpbnB1dCBpZD0iZWFtc0hlbHBlclByZWxvYWRlcl9sYXVuY2giIHR5cGU9YnV0dG9uIGNsYXNzPSJlYW1zX2NsX2J0bjIiIHZhbHVlPSJMYXVuY2giIG9uY2xpY2s9IndpbmRvdy5laEluamVjdGVkLnVpX3ByZXN0YXJ0KCk7IiZndDsmbHQ7L2lucHV0Jmd0Ow0KCQkmbHQ7aW5wdXQgaWQ9ImVhbXNIZWxwZXJQcmVsb2FkZXJfbGF1bmNoIiB0eXBlPWJ1dHRvbiBjbGFzcz0iZWFtc19jbF9idG4yIiB2YWx1ZT0iRGVidWcgKEFkdmFuY2VkKSIgb25jbGljaz0id2luZG93LmVoSW5qZWN0ZWQudWlfcHJlc3RhcnRfanNvbigpOyImZ3Q7Jmx0Oy9pbnB1dCZndDsNCgkmbHQ7L2RpdiZndDsNCgkNCiZsdDsvZGl2Jmd0Ow==");
         var injectContainer = document.createElement('div');
         injectContainer.innerHTML = self.htmlDecode(injectStr);
+        
+        injectContainer.style.zIndex = 99999;
+        injectContainer.style.position = "absolute";
         injectHost.appendChild(injectContainer);
     }
 }
