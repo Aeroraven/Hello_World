49ba0fcb081ea73e57094e66aba777c5b8ea0174
Aeroraven <70007256+Aeroraven@users.noreply.github.com>
2020-11-28 22:42:25 +0800

    Create MOOC_QuickJudge.js

---
+++ MOOC_QuickJudge.js
@@ -0,0 +1,8 @@
+var a=document.getElementsByClassName("j-select");
+for(var i=0;i<a.length;i++){
+	a[i].checked=true;
+}
+var b=document.getElementsByClassName("inputtxt");
+for(var i=1;i<b.length;i++){
+	b[i].value="好";
+}
