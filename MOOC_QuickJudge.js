3d12af657b5e4e1ee8854d716082d74b2dd8d07a
Aeroraven <70007256+Aeroraven@users.noreply.github.com>
2020-11-28 23:00:28 +0800

    Update MOOC_QuickJudge.js

--- MOOC_QuickJudge.js
+++ MOOC_QuickJudge.js
@@ -1,8 +1,34 @@
-var a=document.getElementsByClassName("j-select");
-for(var i=0;i<a.length;i++){
-	a[i].checked=true;
-}
-var b=document.getElementsByClassName("inputtxt");
-for(var i=1;i<b.length;i++){
-	b[i].value="好";
-}
+// ==UserScript==
+// @name         Mooc Fast Mutual Judge
+// @version      0.1
+// @description  Judge as quickly as you can
+// @author       Aeroraven
+// @match        http://www.icourse163.org/learn/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+
+    var s=document.createElement("span");
+    s.style.zIndex=99999;
+    s.innerHTML="Complete Judge";
+    s.style.position="fixed";
+    s.style.left=0;
+    s.style.top=0;
+    s.style.fontSize="17px";
+    s.addEventListener("click",function(){
+        var a=document.getElementsByClassName("j-select");
+        var i=0;
+        for( i=0;i<a.length;i++){
+            a[i].checked=true;
+        }
+        var b=document.getElementsByClassName("inputtxt");
+        for( i=1;i<b.length;i++){
+            b[i].value="好!!! "+Math.random();
+        }
+    });
+    document.getElementsByTagName("body")[0].appendChild(s);
+})();
