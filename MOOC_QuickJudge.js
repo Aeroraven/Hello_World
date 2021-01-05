bf04a45992ed47b73e70a0408e7ddb5c3f578b65
Aeroraven <70007256+Aeroraven@users.noreply.github.com>
2021-01-05 20:45:23 +0800

    Update MOOC_QuickJudge.js

--- MOOC_QuickJudge.js
+++ MOOC_QuickJudge.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name         Mooc Fast Mutual Judge
-// @version      0.1
+// @version      0.1.1
 // @description  Judge as quickly as you can
 // @author       Aeroraven
+// @match        https://www.icourse163.org/learn/*
 // @match        http://www.icourse163.org/learn/*
 // @grant        none
 // ==/UserScript==
@@ -25,9 +26,12 @@
         for( i=0;i<a.length;i++){
             a[i].checked=true;
         }
+        for( i=0;i<a.length;i++){
+            if(Math.random()<0.5){a[i].checked=true;}
+        }
         var b=document.getElementsByClassName("inputtxt");
         for( i=1;i<b.length;i++){
-            b[i].value="好!!! "+Math.random();
+            b[i].value="答案正确";
         }
     });
     document.getElementsByTagName("body")[0].appendChild(s);
