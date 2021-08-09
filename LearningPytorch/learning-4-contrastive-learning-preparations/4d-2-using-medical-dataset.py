e74545253813478ba9b7fcd8d3d572af9064f062
Chloroauric Acid <70007256+Aeroraven@users.noreply.github.com>
2021-08-09 14:04:55 +0800

    Update files

---
+++ LearningPytorch/learning-4-contrastive-learning-preparations/4d-2-using-medical-dataset.py
@@ -0,0 +1,241 @@
+import cv2 as cv
+import torch
+import torch.nn as nn
+import torchvision as torchcv
+import torch.utils.data as torchdata
+import matplotlib.pyplot as plt
+import albumentations as albu
+import numpy as np
+import os
+import PIL.Image as pimg
+import time
+import moco.loader
+import moco.builder as moco_builder
+import math
+import shutil
+import segmentation_models_pytorch as smp
+
+
+def pil_visualize(**kwargs):
+    n = len(kwargs)
+    plt.figure()
+    for i, (name, image) in enumerate(kwargs.items()):
+        plt.subplot(1, n, i + 1)
+        plt.imshow(image)
+        plt.title(name)
+    plt.show()
+
+
+def to_tensor(x, **kwargs):
+    if isinstance(x, torch.Tensor):
+        return x.permute(2, 0, 1).to(torch.float)
+    return x.transpose(2, 0, 1).astype('float32')
+
+
+def get_preprocessing(preprocessing_fn):
+    _transform = [
+        albu.Lambda(image=preprocessing_fn),
+        albu.Lambda(image=to_tensor, mask=to_tensor),
+    ]
+    return albu.Compose(_transform)
+
+
+def pet_argumentation():
+    transform_list = [
+        albu.Resize(100, 100),
+        albu.HorizontalFlip(p=0.5),
+        albu.ToSepia(p=0.2),
+        albu.ToGray(p=0.3),
+        albu.RandomRotate90(p=0.5),
+        albu.VerticalFlip(p=0.2),
+        albu.GaussianBlur(blur_limit=11, p=0.5, always_apply=False)
+    ]
+    return albu.Compose(transform_list)
+
+
+class ArvnDataset_Pet_Constrastive(torchdata.Dataset):
+    def __init__(self,
+                 image_src: str,
+                 classes: list = None,
+                 argumentation: callable = None,
+                 preprocessing: callable = None, ):
+        if classes is None:
+            classes = []
+        self.data_name = []
+        self.classes = classes
+        self.argumentation = argumentation
+        self.preprocessing = preprocessing
+        for i in range(len(self.classes)):
+            file_list = os.listdir(image_src + "/" + self.classes[i])
+            index_list = [i for _ in range(len(file_list))]
+            path_list = [image_src + "/" + self.classes[i] for _ in range(len(file_list))]
+            join_list = zip(file_list, index_list, path_list)
+            self.data_name.extend(join_list)
+        np.random.shuffle(self.data_name)
+
+    def __len__(self):
+        return len(self.data_name)
+
+    def __getitem__(self, item):
+        image = pimg.open(self.data_name[item][2] + "/" + self.data_name[item][0]).convert("RGB")
+        image = np.array(image)
+        image2 = image.copy()
+        label = self.data_name[item][1]
+        if self.argumentation:
+            sp = self.argumentation(image=image)
+            sp2 = self.argumentation(image=image2)
+            image = sp['image']
+            image2 = sp2['image']
+        if self.preprocessing:
+            image = self.preprocessing(image=image)['image']
+            image2 = self.preprocessing(image=image2)['image']
+
+        return [image, image2], label
+
+
+class AverageMeter(object):
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+
+class ProgressMeter(object):
+    def __init__(self, num_batches, meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print('\t'.join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
+
+
+def adjust_learning_rate(optimizer, epoch, arglr, totalepochs):
+    lr = arglr
+    lr *= 0.5 * (1. + math.cos(math.pi * epoch / totalepochs))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+def accuracy(output, target, topk=(1,)):
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].contiguous().view(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
+
+def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, 'model_best.pth.tar')
+
+
+def train(train_loader, model, criterion, optimizer, epoch):
+    batch_time = AverageMeter('Time', ':6.3f')
+    data_time = AverageMeter('Data', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    progress = ProgressMeter(
+        len(train_loader),
+        [batch_time, data_time, losses, top1, top5],
+        prefix="Epoch: [{}]".format(epoch))
+
+    # switch to train mode
+    model.train()
+
+    end = time.time()
+    for i, (images, _) in enumerate(train_loader):
+        data_time.update(time.time() - end)
+        images[0] = images[0].cuda()
+        images[1] = images[1].cuda()
+        output, target = model(im_q=images[0], im_k=images[1])
+        loss = criterion(output, target)
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+        losses.update(loss.item(), images[0].size(0))
+        top1.update(acc1[0], images[0].size(0))
+        top5.update(acc5[0], images[0].size(0))
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % 10 == 0:
+            progress.display(i)
+
+
+class PetNet_V2(nn.Module):
+    def __init__(self, num_classes=128):
+        super(PetNet_V2, self).__init__()
+        self.encoder = smp.Unet(
+            encoder_name="resnet34",
+            encoder_weights="imagenet",
+            classes=2
+        ).encoder
+        self.pool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        self.flatten = nn.Flatten()
+        self.fullconnect = nn.Linear(in_features=512, out_features=128)
+
+    def forward(self, x):
+        x = self.encoder(x)[-1]
+        x = self.pool(x)
+        x = self.flatten(x)
+        x = self.fullconnect(x)
+        return x
+
+
+data_dir = r"D:\2\train\"
+train_dir = data_dir
+preproc_fn = smp.encoders.get_preprocessing_fn("resnet34")
+train_dataset = ArvnDataset_Pet_Constrastive(train_dir, ["imgs"], pet_argumentation(),
+                                             get_preprocessing(preproc_fn))
+train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=64, pin_memory=True, shuffle=False,
+                                               drop_last=True)
+model_base_encoder = PetNet_V2()
+model = moco_builder.MoCo(PetNet_V2, K=1024).to("cuda")
+lr = 1e-3
+loss = nn.CrossEntropyLoss().cuda()
+optimizer = torch.optim.SGD(model.parameters(), lr, momentum=0.9, weight_decay=1e-4)
+epoches = 10
+for epoch in range(epoches):
+    adjust_learning_rate(optimizer, epoch, lr, epoches)
+    train(train_dataloader, model, loss, optimizer, epoch)
+    save_checkpoint({
+        'epoch': epoch + 1,
+        'arch': "resnet34",
+        'state_dict': model.state_dict(),
+        'optimizer': optimizer.state_dict(),
+    }, is_best=False, filename='checkpoint_{:04d}.pth.tar'.format(epoch))
